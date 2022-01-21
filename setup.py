864d57df4c939732b701a3a4cb306f75e284288e
Jordi Adell <jadell@verbio.com>
2022-01-21 17:33:53 +0100

    added the setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythohn-verbio-speech-center',
+    version='0.1.0',
+    packages=[''],
+    url='https://www.speech-center.verbio.com',
+    license='MIT License',
+    author='Verbio Technologies S.L.',
+    author_email='info@verbio.com',
+    description='Verbio Speech Center integration example in python'
+)
