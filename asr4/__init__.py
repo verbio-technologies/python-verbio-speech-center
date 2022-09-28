1fb81548223e1383c548dc31c9391f33ef0b4a1a
rmarrugat <rmarrugat@verbio.com>
2022-09-28 16:21:14 +0200

    Adapted __version__ attribute to new pyproject.

--- asr4/__init__.py
+++ asr4/__init__.py
@@ -1,7 +1,16 @@
 import os
 
 try:
-    from .version import __version__  # noqa
+    import importlib.metadata as importlib_metadata  # noqa
+
+    __version__ = importlib_metadata.version(__package__ or __name__)
 except ImportError:
-    with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "VERSION")) as f:
-        __version__ = f.read().strip()
+    try:
+        import importlib_metadata  # noqa
+
+        __version__ = importlib_metadata.version(__package__ or __name__)
+    except ImportError:
+        with open(
+            os.path.join(os.path.dirname(os.path.dirname(__file__)), "VERSION")
+        ) as f:
+            __version__ = f.read().strip()
