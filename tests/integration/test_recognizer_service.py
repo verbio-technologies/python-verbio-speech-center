bb8aa357c22f6157632c9e3f2b3b0c340a41df50
rmarrugat <rmarrugat@verbio.com>
2022-10-03 17:21:43 +0200

    Formatter integration test.

--- tests/integration/test_recognizer_service.py
+++ tests/integration/test_recognizer_service.py
@@ -10,23 +10,25 @@ from asr4.recognizer import Language
 
 
 class TestRecognizerUtils(object):
-
     def readReference(self, referencePath: str) -> str:
         with open(referencePath) as f:
             return " ".join(f.read().splitlines())
 
-    def runRecognition(
-        self, 
-        audioPath: str, 
-        language: str
-    ) -> Popen:
+    def runRecognition(self, audioPath: str, language: str) -> Popen:
         return Popen(
-            ["python", f"{self.rootdir}/bin/client.py",
-             "--audio-path", audioPath,
-             "--language", language,
-             "--host", self._host,
+            [
+                "python",
+                f"{self.rootdir}/bin/client.py",
+                "--audio-path",
+                audioPath,
+                "--language",
+                language,
+                "--host",
+                self._host,
             ],
-            stdout=PIPE, stderr=PIPE, universal_newlines=True
+            stdout=PIPE,
+            stderr=PIPE,
+            universal_newlines=True,
         )
 
     def checkStatus(self, status: int, stderr: str) -> None:
@@ -42,50 +44,47 @@ class TestRecognizerUtils(object):
         print("\nHypothesis =", hypothesis)
         print("\nReference =", hypothesis)
         print("\nCharacter Error Rate (CER) =", round(cer, 3))
-        print("Word Error Rate (WER) =", round(measures['wer'], 3))
-        print("Match Error Rate (MER) =", round(measures['mer'], 3))
-        print("Word Information Preserved (WIP) =", round(measures['wip'], 3))
-        print("Word Information Lost (WIL) =", round(measures['wil'], 3))
-        print("Correct =", measures['hits'])
-        print("Substitutions =", measures['substitutions'])
-        print("Deletions =", measures['deletions'])
-        print("Insertions =", measures['insertions'])
+        print("Word Error Rate (WER) =", round(measures["wer"], 3))
+        print("Match Error Rate (MER) =", round(measures["mer"], 3))
+        print("Word Information Preserved (WIP) =", round(measures["wip"], 3))
+        print("Word Information Lost (WIL) =", round(measures["wil"], 3))
+        print("Correct =", measures["hits"])
+        print("Substitutions =", measures["substitutions"])
+        print("Deletions =", measures["deletions"])
+        print("Insertions =", measures["insertions"])
 
 
 @pytest.mark.usefixtures("datadir")
 class TestRecognizerService(unittest.TestCase, TestRecognizerUtils):
-
     @pytest.fixture(autouse=True)
     def rootdir(self, pytestconfig):
         self.rootdir = str(pytestconfig.rootdir)
-    
+
     @pytest.fixture(autouse=True)
     def datadir(self, pytestconfig):
         self.datadir = f"{pytestconfig.rootdir}/tests/integration/data"
 
     def setUp(self) -> None:
-        self._language = os.getenv('LANGUAGE', 'en-us')
-        self._hostName = os.getenv('ASR4_HOSTNAME', 'asr4-server')
-        self._hostPort = os.getenv('ASR4_PORT', 50051)
+        self._language = os.getenv("LANGUAGE", "en-us")
+        self._hostName = os.getenv("ASR4_HOSTNAME", "asr4-server")
+        self._hostPort = os.getenv("ASR4_PORT", 50051)
         self._host = f"{self._hostName}:{self._hostPort}"
         self._audio = f"{os.path.join(self.datadir, self._language)}.wav"
-        referencePath = f'{os.path.join(self.datadir, self._language)}.txt'
+        referencePath = f"{os.path.join(self.datadir, self._language)}.txt"
         self._reference = self.readReference(referencePath)
-
 
     def testRecognizeRequest(self):
         process = self.runRecognition(self._audio, self._language)
         status = process.wait(timeout=60)
         self.checkStatus(status, process.stderr.read())
-        
+
         output = process.stdout.read()
-        match = re.search('RecognizeRequest text: \"(.+?)\"', output)
+        match = re.search('RecognizeRequest text: "(.+?)"', output)
         hypothesis = match.group(match.lastindex)
-        
+
         if match != None and match.lastindex != None:
             self.evaluateHypothesis(self._reference, hypothesis)
 
-    
     def testRecognizeRequestWithOtherLanguages(self):
         currentLanguage = Language.parse(self._language)
         for otherLanguage in Language:
@@ -93,11 +92,13 @@ class TestRecognizerService(unittest.TestCase, TestRecognizerUtils):
                 process = self.runRecognition(self._audio, otherLanguage.value)
                 _status = process.wait(timeout=60)
                 output = process.stdout.read()
-                match = re.search(f"Invalid language '{otherLanguage}'. Only '{currentLanguage}' is supported.", output)
+                match = re.search(
+                    f"Invalid language '{otherLanguage}'. Only '{currentLanguage}' is supported.",
+                    output,
+                )
                 self.assertIsNotNone(match)
 
-
     def testEmptyRecognizeRequest(self):
-        process = self.runRecognition(f'{self.datadir}/empty.wav', self._language)
+        process = self.runRecognition(f"{self.datadir}/empty.wav", self._language)
         status = process.wait(timeout=60)
         self.assertEqual(status, 1)
