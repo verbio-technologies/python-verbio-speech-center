ccb2703da09a3bad82130ad86813a39f51abc277
Lluís Formiga i Fanals <lformiga@verbio.com>
2022-11-23 17:51:07 +0100

    Uploading new Dockerfile and init.sh

--- deploy/docker/init.sh
+++ deploy/docker/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-  echo Number of workers not provided
-fi
-
-workers=${1}
+workers="${WORKERS:-default_value}"
 
 python3 server.py -j${workers} -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -f /format-model.$LANGUAGE.fm
