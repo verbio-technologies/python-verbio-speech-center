0f28b0b1847908ab6a412172daafc62e0e531552
Lara Gil <lgil@verbio.com>
2022-11-25 16:51:43 +0100

    fix metric searching

--- tests/e2e/run_metrics.sh
+++ tests/e2e/run_metrics.sh
@@ -32,20 +32,20 @@ function compare_metrics(){
 
 pip install .[client]
 python bin/client.py -l "${language}" --host "${AWS_IP}:50051" -g "${gui}" -m
-sleep 30
+sleep 10
 wer_metric=$(cat "test_${language}_results.tsv" | grep "Accuracy" | cut -d " " -f 2)
-oov_metric=$(jq '.score' "test_${language}_oov.json" |sed 's/[^0-9.]*//g')
+oov_metric=$(jq '.score' "test_${language}_oov.json" | sed 's/[^0-9.]*//g')
 
 
 if [ "${variant}" != "none" ];
 then
 
-	expected_wer=$(jq --arg keyvar "$language-$variant" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
-	expected_oov=$(jq --arg keyvar "$language-$variant" '.[$keyvar].wer' "tests/e2e/data/expected_metrics.json")
+	expected_wer=$(jq --arg keyvar "$language-$variant" '.[$keyvar].accuracy' "tests/e2e/data/expected_metrics.json")
+	expected_oov=$(jq --arg keyvar "$language-$variant" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
 else
-
-	expected_wer=$(jq --arg keyvar "$language" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
-	expected_oov=$(jq --arg keyvar "$language" '.[$keyvar].wer' "tests/e2e/data/expected_metrics.json")
+	echo "$language"
+	expected_wer=$(jq --arg keyvar "$language" '.[$keyvar].accuracy' "tests/e2e/data/expected_metrics.json")
+	expected_oov=$(jq --arg keyvar "$language" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
 fi
 
 echo "Comparing obtained and expected WER metrics..."
