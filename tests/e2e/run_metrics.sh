2bcf8369bad8fab5b5b2df56268c529908e1955f
Verónica López Ludeña <vlopez@verbio.com>
2022-12-02 14:13:46 +0100

    Adding intratest metrics in e2e test and some references (need to be updated)

--- tests/e2e/run_metrics.sh
+++ tests/e2e/run_metrics.sh
@@ -34,29 +34,57 @@ function compare_metrics(){
 pip install .[client]
 python bin/client.py -l "${language}" --host "${AWS_IP}" -g "${gui}" -m
 sleep 10
-accuracy_metric=$(cat "test_${language}_results.tsv" | grep "Accuracy" | cut -d " " -f 2)
-oov_metric=$(jq '.score' "test_${language}_oov.json" | sed 's/[^0-9.]*//g')
 
+accuracy_metric=$(cat "spanish/test_${language}_results.tsv" | grep "Accuracy" | cut -d " " -f 2)
+oov_metric=$(jq '.score' "spanish/test_${language}_oov.json" | sed 's/[^0-9.]*//g')
 
-if [ "${variant}" != "none" ];
-then
+expected_accuracy=$(jq --arg keyvar "$language" '.[$keyvar].accuracy' "tests/e2e/data/expected_metrics.json")
+expected_oov=$(jq --arg keyvar "$language" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
 
-	expected_accuracy=$(jq --arg keyvar "$language-$variant" '.[$keyvar].accuracy' "tests/e2e/data/expected_metrics.json")
-	expected_oov=$(jq --arg keyvar "$language-$variant" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
-else
-	echo "$language"
-	expected_accuracy=$(jq --arg keyvar "$language" '.[$keyvar].accuracy' "tests/e2e/data/expected_metrics.json")
-	expected_oov=$(jq --arg keyvar "$language" '.[$keyvar].oov' "tests/e2e/data/expected_metrics.json")
-fi
-
-echo "Comparing obtained and expected accuracy metrics..."
+echo "Comparing obtained and expected accuracy metrics of $language..."
 compare_metrics ${accuracy_metric} ${expected_accuracy}
 
 echo "Comparing obtained and expected OOV metrics..."
 compare_metrics ${oov_metric} ${expected_oov}
 
+if [ "${language}" == "es" ];
+then
+	dialects=("es" "mx" "co" "pe" "us")
+	for dialect in  "${dialects[@]}";
+	do
+		accuracy_metric=$(jq --arg dialect "$language-$dialect" '.[$dialect]' "spanish/test_${language}_intratest/dialects_intratest.json")
+		expected_accuracy=$(jq --arg lang "$language" --arg dialect "$language-$dialect" '.[$lang].dialects[$dialect].accuracy' "tests/e2e/data/expected_metrics.json")
+
+		echo "Comparing obtained and expected accuracy metrics of $language-$dialect..."
+		compare_metrics ${accuracy_metric} ${expected_accuracy}
+
+		deviation_metric=$(jq --arg dialect "$language-$dialect" '."Accuracy typical deviation"' "spanish/test_${language}_intratest/dialects_intratest.json")
+		expected_deviation=$(jq --arg lang "$language" --arg dialect "$language-$dialect" '.[$lang].dialects["typical_deviation"]' "tests/e2e/data/expected_metrics.json")
+		
+		echo "Comparing obtained and expected accuracy deviation metrics of $language..."
+		compare_metrics ${deviation_metric} ${expected_deviation}
+	done
+fi
+
+domains=($(jq --arg keyvar "$language" '.[$keyvar]' "tests/e2e/data/domains.json" | sed 's/\[//g' | sed 's/\]//g' |  sed 's/"//g'| sed 's/,/ /g' | tr -d '\n' | sed 's/  /  /g'))
+for domain in "${domains[@]}";
+	do
+		accuracy_metric=$(jq --arg dom "$domain" '.[$dom]' "spanish/test_${language}_intratest/domains_intratest.json")
+		expected_accuracy=$(jq --arg lang "$language" --arg dom "$domain" '.[$lang].domains[$dom].accuracy' "tests/e2e/data/expected_metrics.json")
+
+		echo "Comparing obtained and expected accuracy metrics of $domain..."
+		compare_metrics ${accuracy_metric} ${expected_accuracy}
+
+		deviation_metric=$(jq --arg dom "$domain" '."Accuracy typical deviation"' "spanish/test_${language}_intratest/domains_intratest.json")
+		expected_deviation=$(jq --arg lang "$language" --arg dom "$domain" '.[$lang].domains["typical_deviation"]' "tests/e2e/data/expected_metrics.json")
+		
+		echo "Comparing obtained and expected accuracy deviation metrics of $domain..."
+		compare_metrics ${deviation_metric} ${expected_deviation}
+	done
+
 rm "test_${language}_results.tsv"
 rm "test_${language}_oov.json"
+rm -rf "test_${language}_intratest"
 
 if [ $TEST_PASSED == false ];
 then
