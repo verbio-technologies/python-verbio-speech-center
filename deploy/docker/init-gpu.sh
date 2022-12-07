25701e9d6fb1a73079858a5e3904bd34ea6c00cd
Lluís Formiga i Fanals <lformiga@verbio.com>
2022-12-07 12:00:38 +0100

    Changing ${VERSION} for ${ASR4_VERSION}

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+. /opt/intel/oneapi/setvars.sh
+
 workers="${WORKERS:-1}"
 port="${PORT:-50051}"
 LOG_LEVEL=${LOG_LEVEL:-ERROR}
 
-
 CUDA_MODULE_LOADING=EAGER
-
-. /opt/intel/oneapi/setvars.sh
 
 python3 server.py -j${workers} -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
