e667edd7fb424b07036c5260a578a7e68526bfb4
Jordi Adell <jadell@verbio.com>
2022-12-07 19:24:55 +0100

    Logger Service unit testing

---
+++ tests/unit/test_logger_service.py
@@ -0,0 +1,46 @@
+import multiprocessing
+import unittest
+import logging
+import re
+
+
+from asr4.recognizer_v1 import LoggerService, Logger, LoggerQueue
+
+
+class TestLoggerService(unittest.TestCase):
+    def testServiceStops(self):
+        service = LoggerService("INFO", "TestASR4")
+        service.stop()
+
+    def testDefaults(self):
+        self.assertEqual(LoggerService.getDefaultLogLevel(), "ERROR")
+
+    def testLogger(self):
+        queue = multiprocessing.Queue(-1)
+        loggerName = "TestASR4"
+        message = "message"
+        logger = Logger(loggerName, logging.INFO, queue)
+        self.assertEqual(0, queue.qsize())
+        logger.info(message)
+        self.assertEqual(1, queue.qsize())
+        record = queue.get(timeout=5)
+        self.assertEqual(record.name, loggerName)
+        self.assertEqual(record.message, message)
+        self.assertEqual(0, queue.qsize())
+        logger.debug(message)
+        self.assertEqual(0, queue.qsize())
+
+    def testLoggerQueue(self):
+        queue = multiprocessing.Queue(-1)
+        loggerName = "TestASR4"
+        message = "message"
+        loggerQueue = LoggerQueue(loggerName, logging.INFO, queue)
+        self.assertEqual(type(loggerQueue.getLogger()), Logger)
+        loggerQueue.configureGlobalLogger()
+        loggerQueue.getLogger().info(message)
+        self.assertEqual(1, queue.qsize())
+        record = queue.get(timeout=5)
+        self.assertEqual(record.name, loggerName)
+        self.assertEqual(record.message, message)
+
+
