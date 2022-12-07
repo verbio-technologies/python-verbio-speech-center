6e51638b29d5f52acf179ada76a29af9858ec0e7
Lluís Formiga i Fanals <lformiga@verbio.com>
2022-12-07 11:12:13 +0100

    Tag 0.0.30 / Changing base image to development one

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -4,4 +4,9 @@ workers="${WORKERS:-1}"
 port="${PORT:-50051}"
 LOG_LEVEL=${LOG_LEVEL:-ERROR}
 
+
+CUDA_MODULE_LOADING=EAGWE
+
+. /opt/intel/oneapi/setvars.sh
+
 python3 server.py -j${workers} -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
