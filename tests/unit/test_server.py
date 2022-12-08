e28010fd3ff636ffcf1d37f13dccbb0d3e46b616
Jordi Adell <jadell@verbio.com>
2022-12-08 15:54:10 +0100

    added unit tests to test the Server class

---
+++ tests/unit/test_server.py
@@ -0,0 +1,43 @@
+import multiprocessing
+import unittest
+import argparse
+from asr4.recognizer import Server, ServerConfiguration
+from asr4.recognizer import RecognitionServiceConfiguration
+from asr4.recognizer import LoggerService
+
+
+class MockArguments(argparse.Namespace):
+    def __init__(self):
+        super().__init__()
+        self.bindAddress = "bind:address"
+        self.servers = 3
+        self.listeners = 2
+        self.vocabulary = None
+        self.formatter = None
+        self.language = "es"
+        self.model = "model.onnx"
+        self.gpu = False
+        self.workers = 4
+
+
+class TestServerConfiguration(unittest.TestCase):
+    def testConfiguration(self):
+        arguments = MockArguments()
+        configuration = ServerConfiguration(arguments)
+        self.assertEqual(configuration.numberOfListeners, arguments.listeners)
+        self.assertEqual(configuration.numberOfServers, arguments.servers)
+        self.assertEqual(configuration.bindAddress, arguments.bindAddress)
+        self.assertEqual(
+            type(configuration.getServiceConfiguration()),
+            RecognitionServiceConfiguration,
+        )
+
+
+class TestServer(unittest.TestCase):
+    def testServerNoSpawn(self):
+        # It is not possible to test Server because it has not a stop() function
+        multiprocessing.set_start_method("spawn", force=True)
+        loggerService = LoggerService("DEBUG")
+        server = Server(ServerConfiguration(MockArguments()), loggerService)
+        server.stop()
+        server.join()
