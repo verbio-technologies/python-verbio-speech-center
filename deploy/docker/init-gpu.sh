9abb6cb8ce3d9c8e0cccd8e5af2126198f0f736b
Jordi Adell <jadell@verbio.com>
2022-12-14 21:12:00 +0100

    use local variable for workers

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -6,5 +6,5 @@ LOG_LEVEL=${LOG_LEVEL:-ERROR}
 
 CUDA_MODULE_LOADING=EAGER
 
-python3 server.py -s ${WORKERS} -L 1 -w 0 -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
+python3 server.py -s ${workers} -L 1 -w 0 -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
 
