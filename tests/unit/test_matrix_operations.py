5d88610c76254bba08b7d05860d391aa992d74b1
Pere R. Comas <pcomas@verbio.com>
2023-03-03 10:25:53 +0100

    Fix tests for MatrixOperations

--- tests/unit/test_matrix_operations.py
+++ tests/unit/test_matrix_operations.py
@@ -10,20 +10,18 @@ from asr4.recognizer_v1.runtime.onnx import MatrixOperations
 
 class TestMatrixOperation(unittest.TestCase):
     def testSimple(self):
-        m = MatrixOperations()
-
         data = np.array([1,2,3,4,5,6,7,8,9,10,11,12], dtype=np.float32)
 
-        result = m.splitToOverlappingChunks(data, 12, 0)
+        result = MatrixOperations(window=12, overlap=0).splitIntoOverlappingChunks(data)
         self.assertTrue( (result[0]==data).all() )
 
-        result = m.splitToOverlappingChunks(data, 12, 1)
+        result = MatrixOperations(window=12, overlap=1).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (2,12) )
 
-        result = m.splitToOverlappingChunks(data, 12, 2)
+        result = MatrixOperations(window=12, overlap=2).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (2,12) )
 
-        result = m.splitToOverlappingChunks(data, 5, 1)
+        result = MatrixOperations(window=5, overlap=1).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (5,5) )
         self.assertTrue( (result[0] == [0,1,2,3,4]).all() )
         self.assertTrue( (result[1] == [3,4,5,6,7]).all() )
@@ -31,35 +29,32 @@ class TestMatrixOperation(unittest.TestCase):
         self.assertTrue( (result[3] == [9,10,11,12,0]).all() )
         self.assertTrue( (result[4] == [12,0,0,0,0]).all() )
         
-        result = m.splitToOverlappingChunks(data, 4, 1)
+        result = MatrixOperations(window=4, overlap=1).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (7,4) )
 
-        result = m.splitToOverlappingChunks(data, 11, 2)
+        result = MatrixOperations(window=11, overlap=2).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (2,11) )
 
 
     def testExtremes(self):
-        m = MatrixOperations()
-
         data = np.array([1], dtype=np.float32)
 
-        result = m.splitToOverlappingChunks(data, 1, 0)
+        result = MatrixOperations(window=1, overlap=0).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (1,1) )
 
-        result = m.splitToOverlappingChunks(data, 4, 0)
+        result = MatrixOperations(window=4, overlap=0).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (1,4) )
 
         data = np.array([1,2,3,4], dtype=np.float32)
-
-        result = m.splitToOverlappingChunks(data, 4, 0)
+        result = MatrixOperations(window=4, overlap=0).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (1,4) )
 
-        result = m.splitToOverlappingChunks(data, 1, 0)
+        result = MatrixOperations(window=1, overlap=0).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (4,1) )
         
-        result = m.splitToOverlappingChunks(data, 2, 0)
+        result = MatrixOperations(window=2, overlap=0).splitIntoOverlappingChunks(data)
         self.assertEqual( result.shape, (2,2) )
 
         with pytest.raises(AssertionError):
-            m.splitToOverlappingChunks(data, 3, 2)
+            MatrixOperations(window=3, overlap=2).splitIntoOverlappingChunks(data)
 
