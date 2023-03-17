607bb566cf0e981a6eaa70304e48228dcf1ead2c
Pere R. Comas <pcomas@lsi.upc.edu>
2023-03-17 18:18:02 +0100

    Adding LM to the dockerfile machinery

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -5,6 +5,12 @@ port="${PORT:-50051}"
 LOG_LEVEL=${LOG_LEVEL:-ERROR}
 
 CUDA_MODULE_LOADING=EAGER
+export LM="--lm-algorithm viterbi"
 
-python3 server.py -s ${workers} -L 1 -w 0 -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
+if [ -e asr4-${LANGUAGE}-lm.bin ]
+do
+    export LM="--lm-algorithm kenlm --lm-model /asr4-${LANGUAGE}-lm.bin"
+done
+
+python3 server.py -s ${workers} -L 1 -w 0 -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt ${LM} -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
 
--- deploy/docker/init.sh
+++ deploy/docker/init.sh
@@ -7,6 +7,12 @@ LOG_LEVEL=${LOG_LEVEL:-ERROR}
 export OMP_NUM_THREADS=8
 export OMP_WAIT_POLICY=PASSIVE
 export KMP_AFFINITY=scatter
+export LM="--lm-algorithm viterbi"
 
-python3 server.py -s 1 -L ${workers} -w ${OMP_NUM_THREADS} -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port}
+if [ -e asr4-${LANGUAGE}-lm.bin ]
+do
+    export LM="--lm-algorithm kenlm --lm-model /asr4-${LANGUAGE}-lm.bin"
+done
+
+python3 server.py -s 1 -L ${workers} -w ${OMP_NUM_THREADS} -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt ${LM} -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port}
 
