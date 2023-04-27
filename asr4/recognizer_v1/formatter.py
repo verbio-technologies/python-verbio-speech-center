a16ab76b844c08f31b3a4a9a7f537a960ea788b4
Pere R. Comas <pcomas@lsi.upc.edu>
2023-04-27 12:50:20 +0200

    Add fix

--- asr4/recognizer_v1/formatter.py
+++ asr4/recognizer_v1/formatter.py
@@ -5,4 +5,9 @@ from asr4.types.language import Language
 class FormatterFactory:
     @staticmethod
     def createFormatter(model_path: str, language: Language) -> Formatter:
-        return Formatter(language.asFormatter(), model_path, b"", b"", dict())
+        return Formatter(_sanitizeLanguage(language.asFormatter()), model_path, b"", b"", dict())
+
+    def _sanitizeLanguage(language: str) -> str:
+        if len(language)==2:
+            return language+"-"+language
+        return language
