a6336c331ce001999354c1d321400fe87e0c62ef
Verónica López Ludeña <vlopez@verbio.com>
2023-06-13 07:11:58 +0200

    Increasing more the timeout

--- tests/functional/test_recognizer_service.py
+++ tests/functional/test_recognizer_service.py
@@ -467,7 +467,7 @@ class TestRecognizerServiceOnlineDecoding(unittest.TestCase):
         channel = grpc.insecure_channel(
             TestRecognizerServiceOnlineDecoding._serverAddress
         )
-        response = RecognizerStub(channel).Recognize(request, timeout=100)
+        response = RecognizerStub(channel).Recognize(request, timeout=200)
         self.assertEqual(
             response.alternatives[0].transcript,
             DEFAULT_ENGLISH_MESSAGE,
