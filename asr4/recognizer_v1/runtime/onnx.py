db9cbe6474235f6b97540a36d1613cbb6f1aa5d9
Pere R. Comas <pcomas@verbio.com>
2023-06-15 11:56:15 +0200

    Log more

--- asr4/recognizer_v1/runtime/onnx.py
+++ asr4/recognizer_v1/runtime/onnx.py
@@ -345,7 +345,7 @@ class OnnxRuntime(Runtime):
     ) -> OnnxRuntimeResult:
         self._session.logger.debug(" - postprocess")
         (words, timesteps, score) = self._getTimeSteps(output)
-        print("[TIMESTEPS]", timesteps)
+        self._session.logger.debug("[TIMESTEPS] {timesteps}")
         (words, timesteps, frames) = self._performFormatting(
             words, timesteps, enable_formatting
         )
