59d3421b2e647e7dcadc00ae102ffade66ff0807
Verónica López Ludeña <vlopez@verbio.com>
2023-08-25 08:42:22 +0200

    Update docker init for using new toml files with server

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -7,9 +7,5 @@ LOG_LEVEL=${LOG_LEVEL:-ERROR}
 CUDA_MODULE_LOADING=EAGER
 export LM="--lm-algorithm viterbi"
 
-if [ -f asr4-${LANGUAGE}-lm.bin ]; then
-    export LM="--lm-algorithm kenlm --lm-model /asr4-${LANGUAGE}-lm.bin --lm-lexicon /asr4-${LANGUAGE}-lm.lexicon.txt"
-fi
-
-python3 server.py -s ${workers} -L 1 -w 0 -m /asr4-$LANGUAGE.onnx -d /dict.ltr.txt ${LM} -l $LANGUAGE -v "${LOG_LEVEL}" -f /format-model.$LANGUAGE.fm --host [::]:${port} --gpu
+python3 server.py -s 1 -L ${workers} -w ${OMP_NUM_THREADS} -C /asr4_streaming_config_$LANGUAGE.toml -v "${LOG_LEVEL}" --host [::]:${port} --gpu
 
