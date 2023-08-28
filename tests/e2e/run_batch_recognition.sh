b6259c72eb3f2f0955582f0f644c47523ffb54cd
Pere R. Comas <pcomas@lsi.upc.edu>
2023-08-28 17:21:54 +0200

    Add extra parameter to make real streaming

--- tests/e2e/run_batch_recognition.sh
+++ tests/e2e/run_batch_recognition.sh
@@ -4,13 +4,19 @@ set -eEuo pipefail
 language=$1
 host=$2
 audio=$3
+stream=$4
 
 if [[ $language = @(es-es|es-mx|es-co|es-pe|es-us) ]]; then
 	language="es"
 fi
 
 pip install .[client]
-PYTHONPATH=. python bin/client.py --no-format -v ERROR -l "${language}" --host "${host}" -a "${audio}" --batch --json > "${language}-test.json"
+if [ "${stream}" == "stream" ]; then
+    PYTHONPATH=. python bin/client.py --no-format -v ERROR -l "${language}" --host "${host}" -a "${audio}" --json > "${language}-test.json"
+else
+    PYTHONPATH=. python bin/client.py --no-format -v ERROR -l "${language}" --host "${host}" -a "${audio}" --batch --json > "${language}-test.json"
+fi
+
 if [ -z "$(cat ${language}-test.json | grep transcript)" ];
     echo "Transcription is successful"
 then
