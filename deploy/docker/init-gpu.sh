a14d3c95a84ce0d82314b453b9ce07e06b7e4517
Verónica López Ludeña <vlopez@verbio.com>
2023-08-30 08:34:34 +0200

    Remove unused parameter

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -5,7 +5,6 @@ port="${PORT:-50051}"
 LOG_LEVEL=${LOG_LEVEL:-ERROR}
 
 CUDA_MODULE_LOADING=EAGER
-export LM="--lm-algorithm viterbi"
 
 python3 server.py -s ${workers} -L 1 -w 0 -C /asr4_streaming_config_$LANGUAGE.toml -v "${LOG_LEVEL}" --host [::]:${port} --gpu
 
