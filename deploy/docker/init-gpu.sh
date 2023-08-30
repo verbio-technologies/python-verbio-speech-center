17267c490197b95de7a67c07b9b593aad41b1ed3
Verónica López Ludeña <vlopez@verbio.com>
2023-08-30 08:13:38 +0200

    Fix init.sh for gpu

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -7,5 +7,5 @@ LOG_LEVEL=${LOG_LEVEL:-ERROR}
 CUDA_MODULE_LOADING=EAGER
 export LM="--lm-algorithm viterbi"
 
-python3 server.py -s 1 -L ${workers} -w ${OMP_NUM_THREADS} -C /asr4_streaming_config_$LANGUAGE.toml -v "${LOG_LEVEL}" --host [::]:${port} --gpu
+python3 server.py -s ${workers} -L 1 -w 0 -C /asr4_streaming_config_$LANGUAGE.toml -v "${LOG_LEVEL}" --host [::]:${port} --gpu
 
