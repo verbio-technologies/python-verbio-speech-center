3ca620e9aed60f5898fcfd2513bbde44c851d0eb
Pere R. Comas <pcomas@lsi.upc.edu>
2023-10-06 13:55:49 +0200

    Add GPU configuration in the Dockerfile of GPU docker

--- deploy/docker/init-gpu.sh
+++ deploy/docker/init-gpu.sh
@@ -4,9 +4,7 @@ port="${PORT:-50051}"
 LOG_LEVEL=${LOG_LEVEL:-ERROR}
 
 export CUDA_MODULE_LOADING=EAGER
-export W2V_GPU=${USE_GPU:="True"}
-sed -i s/workers\ =\ 8/workers\ =\ 0/ /asr4_streaming_config_$LANGUAGE.toml
-sed -i s/gpu\ =\ false/gpu\ =\ true/ /asr4_streaming_config_$LANGUAGE.toml
+export W2V_GPU="True"
 
-python3 server.py -s ${workers} -C /asr4_streaming_config_$LANGUAGE.toml -v "${LOG_LEVEL}" --host [::]:${port}
+python3 server.py -C /asr4_streaming_config_$LANGUAGE.toml -v "${LOG_LEVEL}" --host [::]:${port}
 
