27dd76b6d15e13cbd9ca0434b9e5d82220070fe5
Guillermo Serrahima <gserrahima@verbio.com>
2024-03-22 16:12:20 +0100

    Added unit test

--- test/test_csr_client.py
+++ test/test_csr_client.py
@@ -3,7 +3,7 @@ sys.path.insert(1, '../proto/generated')
 import pytest
 from unittest.mock import Mock
 from helpers.csr_client import CSRClient
-from helpers.common import RecognizerOptions
+from helpers.common import VerbioGrammar, RecognizerOptions
 from concurrent.futures import ThreadPoolExecutor
 import recognition_streaming_response_pb2 as response
 
@@ -45,3 +45,27 @@ def test_recognition_full_flow_exception():
     with pytest.raises(Exception):
         client.send_audio()
         client.wait_for_response()
+
+
+def test_recognition_full_flow_grammar():
+    mock_stub = Mock()
+    options = RecognizerOptions()
+    options.inactivity_timeout = 0.1
+    options.asr_version = "V2"
+    options.grammar = VerbioGrammar(VerbioGrammar.URI, "test/grammar")
+    options.language = "en-US"
+    options.label = "label"
+    options.formatting = False
+    options.diarization = False
+
+    audio_resource = Mock()
+    audio_resource.sample_rate = 16000
+    audio_resource.audio = b'0000000000000000'
+
+    executor = ThreadPoolExecutor()
+    recognition_result = response.RecognitionResult(is_final=True)
+    mock_response = response.RecognitionStreamingResponse(result=recognition_result)
+    mock_stub.StreamingRecognize.return_value = [mock_response, mock_response]
+    client = CSRClient(executor, mock_stub, options, audio_resource, "token")
+    client.send_audio()
+    client.wait_for_response()
