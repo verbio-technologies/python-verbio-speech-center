7eb5739505c71960dc6ef28e76ca4404a2ea3e77
anikocharyan <akocharyan@verbio.com>
2024-04-10 15:25:29 +0200

    Made conversion to PCM optional by a command line argument --convert-audio. Only for internal use.

--- cli-client/helpers/audio_importer.py
+++ cli-client/helpers/audio_importer.py
@@ -14,11 +14,14 @@ def remove_pcm_audio_file(audio_file: str):
 
 
 class AudioImporter:
-    def __init__(self, audio_file: str):
-        tmp_audio_file = preprocess_audio_file_to_pcm(audio_file)
+    def __init__(self, audio_file: str, convert_audio: bool):
+        tmp_audio_file = audio_file
+        if convert_audio:
+            tmp_audio_file = preprocess_audio_file_to_pcm(audio_file)
         with open(tmp_audio_file, "rb") as wav_file:
             wav_data = wave.open(wav_file)
             self.sample_rate = wav_data.getframerate()
             self.audio = wav_data.readframes(wav_data.getnframes())
             wav_data.close()
-        remove_pcm_audio_file(tmp_audio_file)
+        if convert_audio:
+            remove_pcm_audio_file(tmp_audio_file)
