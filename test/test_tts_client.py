13f1f2e6e567c8f1f84b2fda6996bfe9d74ae441
mjurado <mjurado@verbio.com>
2024-05-14 17:27:38 +0200

    Fixed tests

--- test/test_tts_client.py
+++ test/test_tts_client.py
@@ -1,36 +1,41 @@
 from tts_mocks import TTSStubMockResponse, TTSStubMockCall
 from helpers.tts_client import TTSClient
 from helpers.common import SynthesizerOptions
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock, patch
 
-
-def test_synthesis_full_flow_wav():
+@patch('helpers.audio_exporter.AudioExporter.save_audio')
+def test_synthesis_full_flow_wav(mock_save_audio):
+    mock_save_audio.return_value = "mock audio"
     mock_stub = Mock()
+    mock_executor = MagicMock()
     mock_stub.SynthesizeSpeech.with_call.return_value = (TTSStubMockResponse(), TTSStubMockCall())
     options = SynthesizerOptions()
     options.audio_format = "wav"
-    client = TTSClient(mock_stub, options, "token")
-    audio_samples = client.synthesize("Hello my friend!", "tommy_en_us")
-    assert len(audio_samples) == 24
+    options.text = "Hello"
+    client = TTSClient(mock_executor, mock_stub, options, "token")
+    client.synthesize()
 
 
-def test_synthesis_full_flow_raw():
+@patch('helpers.audio_exporter.AudioExporter.save_audio')
+def test_synthesis_full_flow_raw(mock_save_audio):
+    mock_save_audio.return_value = "mock audio"
     mock_stub = Mock()
+    mock_executor = MagicMock()
     mock_stub.SynthesizeSpeech.with_call.return_value = (TTSStubMockResponse(), TTSStubMockCall())
     options = SynthesizerOptions()
     options.audio_format = "raw"
-    client = TTSClient(mock_stub, options, "token")
-    audio_samples = client.synthesize("Hello my friend!", "miguel_es_pe")
-    assert len(audio_samples) == 24
+    client = TTSClient(mock_executor, mock_stub, options, "token")
+    client.synthesize()
 
 
-def test_synthesis_full_flow_wav_unsecured():
+@patch('helpers.audio_exporter.AudioExporter.save_audio')
+def test_synthesis_full_flow_wav_unsecured(mock_save_audio):
+    mock_save_audio.return_value = "mock audio"
     mock_stub = Mock()
+    mock_executor = MagicMock()
     mock_stub.SynthesizeSpeech.with_call.return_value = (TTSStubMockResponse(), TTSStubMockCall())
     options = SynthesizerOptions()
     options.audio_format = "wav"
     options.secure_channel = False
-    client = TTSClient(mock_stub, options, "token")
-    audio_samples = client.synthesize("Olá, meu amigo", "bel_pt_br")
-    assert len(audio_samples) == 24
-
+    client = TTSClient(mock_executor, mock_stub, options, "token")
+    client.synthesize()
