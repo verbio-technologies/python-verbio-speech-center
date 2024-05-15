78e370bbe67bf8d3f7c953cdd22f6d1ac99bf2bc
mjurado <mjurado@verbio.com>
2024-05-15 12:25:19 +0200

    Move out file save and refactor tests

--- test/test_tts_client.py
+++ test/test_tts_client.py
@@ -12,8 +12,10 @@ def test_synthesis_full_flow_wav(mock_save_audio):
     options = SynthesizerOptions()
     options.audio_format = "wav"
     options.text = "Hello"
+    options.sample_rate = 8000
     client = TTSClient(mock_executor, mock_stub, options, "token")
-    client.synthesize()
+    audio_samples = client.synthesize()
+    assert len(audio_samples) == 24
 
 
 @patch('helpers.audio_exporter.AudioExporter.save_audio')
@@ -24,8 +26,10 @@ def test_synthesis_full_flow_raw(mock_save_audio):
     mock_stub.SynthesizeSpeech.with_call.return_value = (TTSStubMockResponse(), TTSStubMockCall())
     options = SynthesizerOptions()
     options.audio_format = "raw"
+    options.sample_rate = 16000
     client = TTSClient(mock_executor, mock_stub, options, "token")
-    client.synthesize()
+    audio_samples = client.synthesize()
+    assert len(audio_samples) == 24
 
 
 @patch('helpers.audio_exporter.AudioExporter.save_audio')
@@ -36,6 +40,8 @@ def test_synthesis_full_flow_wav_unsecured(mock_save_audio):
     mock_stub.SynthesizeSpeech.with_call.return_value = (TTSStubMockResponse(), TTSStubMockCall())
     options = SynthesizerOptions()
     options.audio_format = "wav"
+    options.sample_rate = 8000
     options.secure_channel = False
     client = TTSClient(mock_executor, mock_stub, options, "token")
-    client.synthesize()
+    audio_samples = client.synthesize()
+    assert len(audio_samples) == 24
